cc1e2e2569d8cb4614159fc83676163ae6838dd1
Lin Yinfeng <lin.yinfeng@outlook.com>
2026-06-13 11:53:14 +0800

    Refactor and add github workflow

---
+++ flake.nix
@@ -0,0 +1,46 @@
+{
+  inputs = {
+    flake-parts.url = "github:hercules-ci/flake-parts";
+    flake-parts.inputs.nixpkgs-lib.follows = "nixpkgs";
+
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+
+    treefmt-nix.url = "github:numtide/treefmt-nix";
+    treefmt-nix.inputs.nixpkgs.follows = "nixpkgs";
+  };
+  outputs =
+    { flake-parts, ... }@inputs:
+    flake-parts.lib.mkFlake { inherit inputs; } {
+      systems = [
+        "x86_64-linux"
+        "aarch64-linux"
+      ];
+      imports = [
+        inputs.treefmt-nix.flakeModule
+      ];
+      perSystem =
+        {
+          self',
+          pkgs,
+          ...
+        }:
+        {
+          packages.slides = pkgs.callPackage ./slides {};
+          checks = self'.packages;
+          treefmt = {
+            projectRootFile = "flake.nix";
+            programs = {
+              nixfmt.enable = true;
+              prettier.enable = true;
+              typstyle = true;
+            };
+          };
+          devShells.default = pkgs.mkShell {
+            env.FONTCONFIG_FILE = pkgs.makeFontsConf { fontDirectories = with pkgs; [ source-han-sans sarasa-gothic ]; };
+            nativeBuildInputs = with pkgs; [
+              typst
+            ];
+          };
+        };
+    };
+}
